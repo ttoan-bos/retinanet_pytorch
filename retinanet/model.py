48660cbce10f90beb419ca0c8736ce732f0808c9
ndttoan <toan.nguyendangthoi@bos-semi.com>
2026-03-06 10:35:15 +0700

    clear screen

--- retinanet/model.py
+++ retinanet/model.py
@@ -56,7 +56,7 @@ class PyramidFeatures(nn.Module):
 
 
         P3_x = self.P3_1(C3)
-        print("Qua conv dau tien: ", P3_x)
+        # #print("Qua conv dau tien: ", P3_x)
         P3_x = P3_x + P4_upsampled_x
         P3_x = self.P3_2(P3_x)
 
@@ -65,7 +65,7 @@ class PyramidFeatures(nn.Module):
         P7_x = self.P7_1(P6_x)
         P7_x = self.P7_2(P7_x)
 
-        print("P3_x:", P3_x)
+        # #print("P3_x:", P3_x)
         return [P3_x, P4_x, P5_x, P6_x, P7_x]
 
 
@@ -145,7 +145,7 @@ class ClassificationModel(nn.Module):
 
         out = self.output(out)
 
-        # print("Before sigmoid:", out)
+        # #print("Before sigmoid:", out)
         out = self.output_act(out)
 
         # out is B x C x W x H, with C = n_classes + n_anchors
@@ -240,7 +240,7 @@ class ResNet(nn.Module):
             img_batch, annotations = inputs
         else:
             img_batch = inputs
-        #print("DEBUG - input shape:", img_batch.shape)
+        ##print("DEBUG - input shape:", img_batch.shape)
         x = self.conv1(img_batch)
         x = self.bn1(x)
         x = self.relu(x)
@@ -252,7 +252,7 @@ class ResNet(nn.Module):
         x4 = self.layer4(x3)
 
         features = self.fpn([x2, x3, x4])
-        #print("DEBUG:\n", x2)
+        ##print("DEBUG:\n", x2)
         regression = torch.cat([self.regressionModel(feature) for feature in features], dim=1)
 
         classification = torch.cat([self.classificationModel(feature) for feature in features], dim=1)
@@ -260,7 +260,7 @@ class ResNet(nn.Module):
         anchors = self.anchors(img_batch)
 
 
-        #print("DEBUG - Raw output shapes: ", classification.shape, regression.shape, anchors.shape)
+        ##print("DEBUG - Raw output shapes: ", classification.shape, regression.shape, anchors.shape)
 
         if self.training:
             return self.focalLoss(classification, regression, anchors, annotations)
